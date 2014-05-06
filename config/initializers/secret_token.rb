bd8dee39b7db144b03d58d1fe8e9c983e79fd9c3
Thomas Karin <thomas.karin@me.com>
2014-05-06 16:51:13 -0400

    Initial commit of rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Busybee::Application.config.secret_key_base = '62896c307b5120f8399038b8a6e826505fddd69fc9625fd97318bb2c4858b816e4eda4a0c428035145d2b416edd39bec741e4555193b599ce38731e34509c11c'
